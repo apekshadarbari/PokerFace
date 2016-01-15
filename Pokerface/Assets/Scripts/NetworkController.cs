771fa0aba298eb4df2f865a82479c48da12a1521
Apeksha Darbari <apekshadarbari@gmail.com>
2016-01-14 18:11:49 -0800

    Steam VR & Photon Plugin code

---
+++ Pokerface/Assets/Scripts/NetworkController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkController : MonoBehaviour {
+
+    private const string roomName = "RoomName";
+    private TypedLobby lobbyName = new TypedLobby("New_Lobby", LobbyType.Default);
+    private RoomInfo[] roomsList;
+    public GameObject player;
+    
+	void Start () {
+        
+        PhotonNetwork.ConnectUsingSettings("v4.2");	
+	}
+	void OnGUI()
+    {
+        if (!PhotonNetwork.connected)
+        {
+            GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+        }
+        else if (PhotonNetwork.room == null)
+        {
+            //create room
+            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+            {
+                 
+                PhotonNetwork.CreateRoom(roomName, new RoomOptions() { maxPlayers = 3, isOpen = true, isVisible = true }, lobbyName);
+            }
+            // Join Room
+            if (roomsList != null)
+            {
+                for (int i = 0; i < roomsList.Length; i++)
+                {
+                    if (GUI.Button(new Rect(100, 250 + (110 * i), 250, 100), "Join " + roomsList[i].name))
+                        PhotonNetwork.JoinRoom(roomsList[i].name);
+                }
+            }
+        }
+    }
+    
+    void OnJoinedLobby(){
+        
+        Debug.Log("Joined Lobby!");
+        RoomOptions roomOptions = new RoomOptions(){};
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+    }
+    
+    void OnJoinedRoom()
+    {
+        Debug.Log("Connected to Room");
+        PhotonNetwork.Instantiate(player.name, Vector3.up * 5, Quaternion.identity, 0);
+    }
+}
---
+++ Pokerface/Assets/Scripts/NetworkedPlayer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkedPlayer : Photon.MonoBehaviour {
+
+	// Use this for initialization
+    
+    public GameObject avatar;
+    public Transform playerGlobal;
+    public Transform playerLocal;
+	
+    void Start () {
+	
+        Debug.Log("I'm instantiated!");
+        
+        
+        
+	}
+    // 
+    void Update(){
+                
+ 
+        
+        if (photonView.isMine)
+        {
+        Debug.Log("player is mine");
+
+
+        
+       // avatar.SetActive(false);
+        
+        }
+      
+    }
+	
+	// Update is called once per frame
+	void onPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info) {
+        
+        if(stream.isWriting)
+        {
+            stream.SendNext(playerGlobal.position);
+            stream.SendNext(playerGlobal.rotation);
+            stream.SendNext(playerLocal.localPosition);
+            stream.SendNext(playerLocal.localRotation);
+        }	
+        else {
+            
+            this.transform.position = (Vector3)stream.ReceiveNext();
+            this.transform.rotation = (Quaternion)stream.ReceiveNext();
+            avatar.transform.localPosition = (Vector3)stream.ReceiveNext();
+            avatar.transform.localRotation = (Quaternion)stream.ReceiveNext();
+            
+        }
+	}
+}
