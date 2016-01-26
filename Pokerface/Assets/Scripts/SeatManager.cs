9f652b6bb738764cd232d63fab8662ae28e0ff81
jacob kjeldsen <jacoblbkjeldsen@gmail.com>
2016-01-25 16:28:27 -0900

    File fix 0.1

--- Pokerface/Assets/Scripts/SeatManager.cs
+++ Pokerface/Assets/Scripts/SeatManager.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,8 +5,8 @@ using System.Linq;
 
 public class SeatManager : MonoBehaviour
 {
-    
-    
+
+
     private static SeatManager instance;
 
     private Occupied[] seats;
@@ -35,7 +34,7 @@ public class SeatManager : MonoBehaviour
 
     public bool IsSeatAvailable()
     {
-        return isOccupied.Any(b => !b); 
+        return isOccupied.Any(b => !b);
     }
 
     public int GetFirstAvailableSeat()
@@ -50,56 +49,3 @@ public class SeatManager : MonoBehaviour
         return -1;
     }
 }
-=======
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-
-public class SeatManager : MonoBehaviour
-{
-    
-    
-    private static SeatManager instance;
-
-    private Occupied[] seats;
-
-    private bool[] isOccupied;
-
-    public static SeatManager Instance
-    {
-        get
-        {
-            return instance;
-        }
-    }
-
-    private void Start()
-    {
-        instance = this;
-        seats = transform.GetComponentsInChildren<Occupied>();
-        isOccupied = new bool[seats.Length];
-        for (int i = 0; i < isOccupied.Length; i++)
-        {
-            isOccupied[i] = true;
-        }
-    }
-
-    public bool IsSeatAvailable()
-    {
-        return isOccupied.Any(b => !b); 
-    }
-
-    public int GetFirstAvailableSeat()
-    {
-        for (int i = 0; i < isOccupied.Length; i++)
-        {
-            if (!isOccupied[i])
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-}
->>>>>>> da1a6e2190ac886a62af1e2b273e58d39b9f820d
