b543dbf8d34900cf75661bb5a36fc8063e70ba1f
unknown <kkinnunen@PCVR.roadhouse.local>
2016-02-22 15:14:24 -0800

    crosshair changes from htc vive to oculus rift

--- Pokerface/Assets/Scripts/Utilities/VRInputManager.cs
+++ Pokerface/Assets/Scripts/Utilities/VRInputManager.cs
@@ -1,18 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class VRInputManager : MonoBehaviour {
+public class VRInputManager : MonoBehaviour
+{
 
     SteamVR vr;
 
+    [SerializeField]
+    GameObject crosshair;
+
+    [SerializeField]
+    GameObject seat;
+
     void Start()
     {
+        //null for rift
         vr = SteamVR.instance;
-        string hmdModel = vr.hmd_ModelNumber.ToString();
-        //Vive DVT
-        string hmdTracker = vr.hmd_TrackingSystemName.ToString();
-        //lighthouse
-        Debug.Log(hmdModel);
-        Debug.Log(hmdTracker);
+        
+        //lighthouse if vive
+        string hmd = vr.hmd_TrackingSystemName.ToString();
+
+        if (hmd == "lighthouse")
+        {
+            //crosshair if gaze on enable crosshair on head 
+            //seats set to Y.0
+            crosshair = GameObject.FindGameObjectWithTag("CrosshairVive");
+            crosshair.GetComponent<MeshRenderer>().enabled = true;
+            Debug.Log("Vive");
+        }
+        else
+        {
+            //gaze on eye
+            //raycast from eye?
+            //
+            crosshair = GameObject.FindGameObjectWithTag("CrosshairRift");
+            crosshair.GetComponent<MeshRenderer>().enabled = true;
+            Debug.Log("Not Vive");
+        }
+
+        Debug.Log(hmd);
     }
 }
