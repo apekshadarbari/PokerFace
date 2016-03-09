685b89ae843582f033d1739f4d9fe60f13c5816d
jacob kjeldsen <jacoblbkjeldsen@gmail.com>
2016-03-09 10:49:27 -0800

    no message

---
+++ Pokerface/Assets/Scripts/Billin/PawActions.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PawActions : MonoBehaviour {
+    public GameObject greenBtn;
+    public GameObject foldBtn;
+    public GameObject chipSpotlight;
+
+    public GameObject foldHUD;
+    public GameObject checkHUD;
+    public GameObject callHUD;
+    public GameObject raiseHUD;
+    public GameObject walletHUD;
+
+    public static bool isMyTurn;
+    public static bool isCheck;
+    public static bool isCall;
+
+    void Awake() {
+        foldHUD.SetActive(false);
+        checkHUD.SetActive(false);
+        callHUD.SetActive(false);
+        raiseHUD.SetActive(false);
+        walletHUD.SetActive(false);
+    }
+
+    void Start()
+    {
+        Debug.Log("Your opponent's turn!");
+        StartCoroutine(beforeMyTurn());
+        chipSpotlight.GetComponent<Light>().enabled = false;
+    }
+  
+    IEnumerator beforeMyTurn()
+    {
+        yield return new WaitForSeconds(5f);
+        isMyTurn = true;
+        if (Random.value < 0.5f)
+        {
+            Debug.Log("Your opponent checks!");
+            isCheck = true;
+        }
+        else {
+            Debug.Log("Your opponent raises!");
+            isCall = true;
+        }
+    }
+
+    void Update() {
+        if (isMyTurn) {
+            greenBtn.GetComponent<Renderer>().material.color = Color.white;
+            foldBtn.GetComponent<Renderer>().material.color = Color.white;
+            foldHUD.SetActive(true);
+            walletHUD.SetActive(true);
+            chipSpotlight.GetComponent<Light>().enabled = true;
+
+            if (isCheck)
+            {
+                checkHUD.SetActive(true);
+            }
+            else if (isCall)
+            {
+                callHUD.SetActive(true);
+            }
+            else {
+                if (ChipMove.chipClicked)
+                {
+                    checkHUD.SetActive(false);
+                    raiseHUD.SetActive(true);
+                }
+            }
+        }
+    }
+}
---
+++ Pokerface/Assets/Scripts/Environment/TurnIndicator.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TurnIndicator : PhotonManager<TurnIndicator>
+{
+    //[SerializeField]
+    //private Transform indicatorTrans;
+
+    private int player;
+
+    [SerializeField]
+    private float speed = 1f;
+
+    [SerializeField, Header("Turn( mulitplied by speed)")]
+    private float degreesPerUpdate = 180f;
+
+    private Vector3 targetPos;
+
+    // Use this for initialization
+    private void Start()
+    {
+        TurnIndication(1);
+        transform.position = targetPos;
+        //indicatorTrans = this.gameObject.transform;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (transform.position != targetPos)
+        {
+            Vector3 dir = (targetPos - transform.position).normalized;
+            transform.rotation *= Quaternion.AngleAxis((degreesPerUpdate * speed) * Time.smoothDeltaTime, Vector3.Cross(Vector3.up, dir)); // change degrees up for faster roll
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.smoothDeltaTime);
+            //transform.Rotate(1, 0, 1);
+        }
+    }
+
+    //take current players turn and lerp toward the position
+    public void TurnIndication(int player)
+    {
+        this.player = player;
+        if (this.player == 1)
+        {
+            targetPos = new Vector3(.78f, 1.86f, -.33f);
+        }
+        else if (this.player == 2)
+        {
+            targetPos = new Vector3(-.151f, 1.86f, 1.127f);
+        }
+        //Debug.Log("TURNINDICATION COMMENSING");
+    }
+
+    private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(gameObject.transform.position);
+            stream.SendNext(gameObject.transform.rotation); // if we add ball rolling later
+        }
+        else
+        {
+            this.transform.position = (Vector3)stream.ReceiveNext();
+            this.transform.rotation = (Quaternion)stream.ReceiveNext(); // if we add ball rolling later
+        }
+    }
+}
